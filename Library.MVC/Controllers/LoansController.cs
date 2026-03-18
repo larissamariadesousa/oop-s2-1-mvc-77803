b630047dd19496a2cb3cc3ff256a0ba16e220a6b
larissamariadesousa <77803@student.dorset-college.ie>
2026-03-18 13:10:38 +0000

    memberupdate

--- Library.MVC/Controllers/LoansController.cs
+++ Library.MVC/Controllers/LoansController.cs
@@ -14,22 +14,68 @@ public class LoansController : Controller
         _context = context;
     }
 
+    // GET: Loans/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create(Loan loan)
     {
+        if (!ModelState.IsValid)
+        {
+            return View(loan);
+        }
+
+     
         var activeLoan = await _context.Loans
             .FirstOrDefaultAsync(l => l.BookId == loan.BookId && l.ReturnedDate == null);
 
         if (activeLoan != null)
         {
-            return BadRequest("Book already on loan");
+            ModelState.AddModelError("", "This book is already on loan.");
+            return View(loan);
         }
 
+    
         loan.LoanDate = DateTime.Now;
 
-        _context.Add(loan);
+        _context.Loans.Add(loan);
+        
+        var book = await _context.Books.FindAsync(loan.BookId);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        book.IsAvailable = false;
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index", "Books");
+    }
+    
+    public async Task<IActionResult> Return(int id)
+    {
+        var loan = await _context.Loans.FindAsync(id);
+
+        if (loan == null)
+        {
+            return NotFound();
+        }
+
+        loan.ReturnedDate = DateTime.Now;
 
         var book = await _context.Books.FindAsync(loan.BookId);
-        book.IsAvailable = false;
+
+        if (book != null)
+        {
+            book.IsAvailable = true;
+        }
 
         await _context.SaveChangesAsync();
 
